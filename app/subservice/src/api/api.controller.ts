afe36eba21b48f79b01fcba729e95135ee5a4809
lolka0d-u <sn24008@edu.lu.lv>
2024-12-15 23:54:46 +0200

    Deleted unused imports

--- app/subservice/src/api/api.controller.ts
+++ app/subservice/src/api/api.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { getBalance } from './sol_functions';
 
 @Controller('api')
